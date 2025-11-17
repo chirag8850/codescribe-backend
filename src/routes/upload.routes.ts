7e58647ed587a0df0ff88960ac76a524574602d0
chirag8850 <c.vaviya@somaiya.edu>
2025-11-17 22:29:48 +0530

    created two seperate router to upload image one with authenticated middleware and one without

--- src/routes/upload.routes.ts
+++ src/routes/upload.routes.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import { uploadController }  from '../controllers/file.controller';
 import { upload, handleMulterError } from '../middlewares/multer.middleware';
+import { authenticateToken } from '../middlewares/auth.middleware';
 
 const router = express.Router();
 
-router.post('/image', upload.single('file'), handleMulterError, uploadController);
+router.post('/avatar', upload.single('file'), handleMulterError, uploadController);
+
+router.post('/image', authenticateToken, upload.single('file'), handleMulterError, uploadController);
 
 export default router;
